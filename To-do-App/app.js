29554cf8e46169c0733c50dee1bbf6869cdf9a54
Priyanka Kumari <64683009+decodepriyanka@users.noreply.github.com>
2022-10-10 17:48:26 +0530

    Add files via upload

---
+++ To-do-App/app.js
@@ -0,0 +1,121 @@
+// DOM Vars
+const addBtn = document.querySelector(".rightHead");
+const container = document.querySelector(".container");
+const itemList = document.querySelector(".itemList");
+const emptyList = document.querySelector(".emptyList");
+const popUpItem = document.querySelector(".popUpItem");
+const popUpItemList = document.querySelector(".popUpItemList");
+const closeBtn = document.querySelector("#closeBtn");
+const list = document.querySelector("#listVal");
+const item = document.querySelector("#itemVal");
+
+//initial dynamic style
+popUpItem.style.display = "none";
+popUpItemList.style.display = "none";
+
+var insertionPlace;
+
+// event on add button initially
+addBtn.addEventListener("click", function () {
+  if (popUpItem.style.display == "none") {
+    popUpItem.style.display = "block";
+  }
+  container.classList.add("active");
+});
+
+// event on close button of pop up initially
+closeBtn.addEventListener("click", function () {
+  if (popUpItem.style.display == "block") {
+    popUpItem.style.display = "none";
+  }
+  container.classList.remove("active");
+  list.value = "";
+});
+
+// event on add button of popup item
+//creating header of each List
+document.addEventListener("click", function (e) {
+  if (e.target && e.target.id == "addBtn") {
+    popUpItem.style.display = "none";
+    container.classList.remove("active");
+    const listVal = list.value;
+    list.value = "";
+    if (listVal) {
+      emptyList.style.display = "none";
+    }
+
+    const newListItem = document.createElement("div");
+    newListItem.className = "listItem";
+    newListItem.innerHTML = `
+        <header>${listVal}</header> 
+        <ul id="listContent">
+        </ul>
+        <i class="fas fa-plus-circle" id="addItemBtn"></i>
+        <i class="far fa-trash-alt" id="closeItemBtn"></i>
+        `;
+    itemList.appendChild(newListItem);
+  }
+});
+
+// event on add button to list new item
+//on click add btn in list
+document.addEventListener("click", function (e) {
+  if (e.target && e.target.id == "addItemBtn") {
+    if (popUpItemList.style.display == "none") {
+      popUpItemList.style.display = "block";
+    }
+    container.classList.add("active");
+
+    //take ul tag from each list
+    insertionPlace = e.target.parentElement.childNodes[3];
+  }
+});
+
+// event for adding new item in the list
+// add item in list
+document.addEventListener("click", function (e) {
+  if (e.target && e.target.id == "addItem") {
+    popUpItemList.style.display = "none";
+    container.classList.remove("active");
+    const itemVal = item.value;
+    item.value = "";
+    const li = document.createElement("li");
+    li.className = "eachItem";
+    const text = document.createTextNode(itemVal);
+    li.appendChild(text);
+    const a = document.createElement("a");
+    a.id = "markDone";
+    const linkText = document.createTextNode("mark done");
+    a.appendChild(linkText);
+    li.appendChild(a);
+    insertionPlace.appendChild(li);
+  }
+});
+
+// event for close button while adding new item in the list
+document.addEventListener("click", function (e) {
+  if (e.target && e.target.id == "closeItem") {
+    popUpItemList.style.display = "none";
+    container.classList.remove("active");
+    item.value = "";
+  }
+});
+
+// event for deleting the list
+document.addEventListener("click", function (e) {
+  if (e.target && e.target.id == "closeItemBtn") {
+    e.target.parentElement.remove();
+    if (!itemList.getElementsByTagName("div").length) {
+      emptyList.style.display = "block";
+    }
+  }
+});
+
+// event for marking the item
+document.addEventListener("click", function (e) {
+  if (e.target && e.target.id == "markDone") {
+    const parent = e.target.parentElement;
+    parent.classList.add("afterMark");
+    e.target.remove();
+  }
+});
